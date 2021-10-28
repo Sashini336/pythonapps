1a94c7cd32a705e6122791ad0d65e8b43a4bc51f
Sashini336 <46651489+Sashini336@users.noreply.github.com>
2021-10-28 12:42:53 +0300

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -5,6 +5,8 @@ data = json.load(open("data.json"))
 def translate(word):
     if word in data:
      return data[word]
+    else:
+        print("You have entered wrong word please check it again")
 
 word = input("Enter the word you want to search: ")
 output = translate(word)
